6c49a240414b7e63a0d6d405dcf3ed5d2d816f68
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-03-19 14:57:11 -0600

    feat: variables de entorno

---
+++ config.js
@@ -0,0 +1,8 @@
+// export const VITE_API_URL = import.meta.env.VITE_API_URL;
+// export const VITE_XYZ = import.meta.env.VITE_XYZ;
+// const x = import.meta.env.VITE_API_URL;
+// const z = import.meta.env.VITE_SOME_KEY;
+// console.log(process.env);
+// debugger;
+export const VITE_API_URL = process.env.VITE_API_URL;
+export const API_URL = process.env.API_URL;
