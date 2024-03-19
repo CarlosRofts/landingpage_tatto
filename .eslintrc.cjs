ed9c54c9b1af979c44d045eb0087ed01a12d8f5f
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-03-19 14:42:32 -0600

    chore: .eslintrc.js reset a warnings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: 'airbnb-base',
+	extends: 'eslint:recommended',
 	overrides: [
 		{
 			env: {
@@ -20,17 +20,7 @@ module.exports = {
 		sourceType: 'module',
 	},
 	rules: {
-		'no-new': 'off',
-		'no-param-reassign': 'off',
-		'no-console': 'off',
 		'linebreak-style': ['warn', 'unix'],
-		'no-restricted-syntax': [
-			'warn',
-			{
-				selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
-				message: 'Unexpected property on console object was called',
-			},
-		],
 		indent: ['warn', 2, { SwitchCase: 1 }],
 		quotes: ['warn', 'single'],
 		semi: ['warn', 'always'],
