094608956d0b1c3af43b9a293ea43d87b5c982e6
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-05-07 17:00:59 -0600

    refactor: codigo comentado

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-const base = document.createElement('base');
-base.href = import.meta.env.BASE_URL;
-document.head.insertBefore(base, document.head.firstChild);
+// const base = document.createElement('base');
+// base.href = import.meta.env.BASE_URL;
+// document.head.insertBefore(base, document.head.firstChild);
 
 import '../css/normalize.css';
 import '../css/main.css';
