c6326064250cac1569779109a06376439e37295a
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-05-07 16:54:26 -0600

    refactor: mpa config a vite.conf

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,33 @@ import { defineConfig, loadEnv } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 import babel from 'vite-plugin-babel';
 import glsl from 'vite-plugin-glsl';
+import { resolve } from 'path';
 // import dotenv from 'dotenv';
 
 export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	return {
-		base: '/landingpage_tatto/', // // ⛔ githubpages
-		// base: './', // ⛔ local
+		// base: '/landingpage_tatto/', // // ⛔ githubpages
+		base: './', // ⛔ local
 		root: 'src/',
 		publicDir: '../public/',
+		// assetsInclude: ['**/*.html', '!index.html'],
 		plugins: [
+			// glsl({
+			// 	include: [
+			// 		// Glob pattern, or array of glob patterns to import
+			// 		'**/*.glsl',
+			// 		'**/*.wgsl',
+			// 		'**/*.vert',
+			// 		'**/*.frag',
+			// 		'**/*.vs',
+			// 		'**/*.fs',
+			// 	],
+			// 	// defaultExtension: 'glsl', // Shader suffix when no extension is specified
+			// 	// compress: false, // Compress output shader code
+			// 	// watch: true, // Recompile shader on change
+			// 	// root: 'src/', // Directory for root imports
+			// }),
 			eslintPlugin({
 				cache: true,
 				fix: true,
@@ -34,8 +51,8 @@ export default defineConfig(({ command, mode }) => {
 			polyfillDynamicImport: false,
 			outDir: '../dist',
 			assetsDir: '',
-			minify: 'terser',
 			sourcemap: true,
+			minify: 'terser',
 			rollupOptions: {
 				external: [],
 				output: {
@@ -44,6 +61,10 @@ export default defineConfig(({ command, mode }) => {
 							return 'vendor';
 						}
 					},
+				},
+				input: {
+					main: resolve(__dirname, 'src/index.html'),
+					nested: resolve(__dirname, 'src/pages/gallery.html'),
 				},
 			},
 		},
