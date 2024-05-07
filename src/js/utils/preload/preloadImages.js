d803b9eb2ebfb5a378bc7f52e56c0c02018519c0
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-05-07 10:59:36 -0600

    feat: preloadImages

---
+++ src/js/utils/preload/preloadImages.js
@@ -0,0 +1,9 @@
+// Preload images
+import imagesLoaded from 'imagesloaded';
+
+const preloadImages = (selector = 'img') => {
+  return new Promise((resolve) => {
+    imagesLoaded(document.querySelectorAll(selector), {background: true}, resolve);
+  });
+};
+export default preloadImages;
